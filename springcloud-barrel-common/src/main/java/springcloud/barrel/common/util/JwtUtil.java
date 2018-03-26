49e6517034ed6d13499d9ba5300b68b55fb8c592
chenjinquan <chenjinquan@banggood.com>
2018-03-26 13:45:04 +0800

    Add JwtUtil

--- springcloud-barrel-common/src/main/java/springcloud/barrel/common/util/JwtUtil.java
+++ springcloud-barrel-common/src/main/java/springcloud/barrel/common/util/JwtUtil.java
@@ -1,14 +1,13 @@
 package springcloud.barrel.common.util;
 
 import com.alibaba.fastjson.JSON;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.jwt.Jwt;
 import org.springframework.security.jwt.JwtHelper;
 import org.springframework.security.jwt.crypto.sign.MacSigner;
 import org.springframework.security.jwt.crypto.sign.SignatureVerifier;
 import org.springframework.util.StringUtils;
 
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 /**
@@ -16,24 +15,34 @@ import java.util.Map;
  * @date 2018/3/25
  */
 public class JwtUtil {
-    JwtHelper jwtHelper = new JwtHelper();
 
     static final String OAUTH2_TOKEN_PREFIX = "Bearer ";
     static final String TOKEN_HEADER = "Authorization";
 
-    public static Map<String, Object> parseTokenBySHA256(HttpServletResponse response, String key) throws Exception {
-        final String token = response.getHeader(TOKEN_HEADER);
+
+    public static JwtPayLoad parseTokenBySHA256(HttpServletRequest request, String key) {
+        final String token = request.getHeader(TOKEN_HEADER);
+        return parseTokenBySHA256(token, key);
+    }
+
+    public static JwtPayLoad parseTokenBySHA256(String token, String key) {
         Jwt jwt = parseOauth2TokenBySHA256(token, key);
-        return jsonToMap(jwt.getClaims());
+        final Map<String, Object> map = jsonToMap(jwt.getClaims());
+        return new JwtPayLoad() {
+            @Override
+            public Map<String, Object> payloadMap() {
+                return map;
+            }
+        };
     }
 
     public static Map<String, Object> jsonToMap(String json) {
-        Map<String, Object> map = JSON.parseObject(json).getInnerMap();
+        Map<String, Object> map = JSON.parseObject(json);
         return map;
     }
 
 
-    public static Jwt parseOauth2TokenBySHA256(String token, String key) throws Exception {
+    public static Jwt parseOauth2TokenBySHA256(String token, String key) {
         final String oauth2Token = token.trim();
         verifyTokenFormat(oauth2Token);
         final String tokenContext = oauth2Token.substring(OAUTH2_TOKEN_PREFIX.length()).trim();
@@ -42,7 +51,7 @@ public class JwtUtil {
         return jwt;
     }
 
-    private static void verifyTokenFormat(String oauth2Token) throws Exception {
+    private static void verifyTokenFormat(String oauth2Token) {
         if (StringUtils.isEmpty(oauth2Token)) {
             throwTokenIsEmptyException();
         }
@@ -52,29 +61,13 @@ public class JwtUtil {
     }
 
 
-    private static void throwTokenIsEmptyException() throws Exception {
-        throw new Exception("Token is empty");
+    private static void throwTokenIsEmptyException() {
+        throw new RuntimeException("Token is empty");
     }
 
-    private static void throwIsNotOauth2TokenException() throws Exception {
-        throw new Exception("Token is not a oauth2Token");
-    }
-
-    private static void throwTokenIsNotValidException() throws Exception {
-        throw new Exception("Token is not valid");
+    private static void throwIsNotOauth2TokenException() {
+        throw new RuntimeException("Token is not a oauth2Token");
     }
 
 
-    public static void main(String[] args) throws Exception {
-        String token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9" +
-                ".eyJleHAiOjE1MjIwNjM2OTUsInVzZXJfbmFtZSI6ImNoZW5qaW5x" +
-                "dWFuIiwiYXV0aG9yaXRpZXMiOlsicm9vdCJdLCJqdGkiOiJjYTAwN" +
-                "2U3Ni0yYjljLTRkNDUtYmUzZC00Y2UwZDAxYTAzYjUiLCJjbGllbnR" +
-                "faWQiOiJhcGkiLCJzY29wZSI6WyJ4eDEiXX0" +
-                ".ZMIgPow1SMdMW6d2823Nliide7BcLTz3bEXwXJ92N_g";
-        Jwt jwt = parseOauth2TokenBySHA256(token, "123");
-        System.out.println(jsonToMap(jwt.getClaims()));
-        System.out.println(jwt.getClaims());
-
-    }
 }
